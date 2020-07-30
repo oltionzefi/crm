062891d2a881e6826743fc559349d88c8cec2ec9
Oltion Zefi <oltionzefi@gmail.com>
2020-07-30 11:46:31 +0200

    Upgrade packages

--- apps/crm/jest.config.js
+++ apps/crm/jest.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  name: 'crm',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/crm',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
+	name: 'crm',
+	preset: '../../jest.config.js',
+	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+	globals: {
+		'ts-jest': {
+			tsConfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.(html|svg)$',
+			astTransformers: [
+				'jest-preset-angular/build/InlineFilesTransformer',
+				'jest-preset-angular/build/StripStylesTransformer',
+			],
+		},
+	},
+	coverageDirectory: '../../coverage/apps/crm',
+	snapshotSerializers: [
+		'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+		'jest-preset-angular/build/AngularSnapshotSerializer.js',
+		'jest-preset-angular/build/HTMLCommentSerializer.js',
+	],
 };
