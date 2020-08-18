c240382e0debf137cdab5f9816d0651ac396a074
Oltion Zefi <oltionzefi@gmail.com>
2020-08-19 01:12:23 +0200

    Specs for loading module

--- apps/crm/src/app/loading/interceptors/loader.interceptor.spec.ts
+++ apps/crm/src/app/loading/interceptors/loader.interceptor.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoaderInterceptor } from './loader.interceptor';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoaderInterceptor', () => {
 	let service: LoaderInterceptor;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
 			providers: [LoaderInterceptor],
 		});
 		service = TestBed.inject(LoaderInterceptor);
--- apps/crm/src/app/loading/loading.component.spec.ts
+++ apps/crm/src/app/loading/loading.component.spec.ts
@@ -1,26 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+	async,
+	ComponentFixture,
+	TestBed,
+	fakeAsync,
+	tick,
+} from '@angular/core/testing';
 
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { LoadingComponent } from './loading.component';
+import { By } from '@angular/platform-browser';
+import { LoadingService } from './services/loading.service';
 
 describe('LoadingComponent', () => {
 	let component: LoadingComponent;
 	let fixture: ComponentFixture<LoadingComponent>;
+	let service: LoadingService;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [ProgressSpinnerModule],
 			declarations: [LoadingComponent],
+			providers: [LoadingService],
 		}).compileComponents();
 	}));
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(LoadingComponent);
 		component = fixture.componentInstance;
+		service = TestBed.inject(LoadingService);
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('should not render loading spinner', () => {
+		const spinner = fixture.debugElement.query(By.css('p-progressSpinner'));
+		expect(spinner).toBeFalsy();
+	});
+
+	it('should render spinner when loading did change to true', fakeAsync(() => {
+		service.isLoading.next(true);
+		tick();
+		fixture.detectChanges();
+		const spinner = fixture.debugElement.query(By.css('p-progressSpinner'))
+			.nativeElement;
+
+		expect(spinner).toBeTruthy();
+	}));
+
+	it('should stop rendering spinner when loading did change to false', fakeAsync(() => {
+		service.isLoading.next(false);
+		tick();
+		fixture.detectChanges();
+		const spinner = fixture.debugElement.query(By.css('p-progressSpinner'));
+
+		expect(spinner).toBeFalsy();
+	}));
 });
