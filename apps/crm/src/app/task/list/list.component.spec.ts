c2735818e5c45cb61bbf15f7f6bce514e020a1e4
Oltion Zefi <oltionzefi@gmail.com>
2023-01-14 20:56:33 +0100

    Adjust specs for task list

--- apps/crm/src/app/task/list/list.component.spec.ts
+++ apps/crm/src/app/task/list/list.component.spec.ts
@@ -3,8 +3,13 @@ import { SplitButtonModule } from 'primeng/splitbutton';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { TableModule } from 'primeng/table';
 
 import { ListComponent } from './list.component';
+import { TaskService } from '../services';
+import { TaskServiceMock } from '../mocks';
+import { ActivatedRouteMock } from '../../mocks';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -12,8 +17,24 @@ describe('ListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([]), ToolbarModule, ButtonModule, SplitButtonModule],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        ToolbarModule,
+        ButtonModule,
+        SplitButtonModule,
+        TableModule,
+      ],
       declarations: [ListComponent],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: TaskServiceMock,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: ActivatedRouteMock,
+        },
+      ],
     }).compileComponents();
   }));
 
